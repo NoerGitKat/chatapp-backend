d8a9a6f191b5b123975fbec4b5764e70b5319b5d
Noer Paanakker <noer@lightworksweb.nl>
2023-03-19 00:01:31 +0100

    Setup security and standard middleware

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const HOUR = 3600000;
+export const DAY = 24 * HOUR;
+export const WEEK = 7 * DAY;
--- src/utils/setupServer.ts
+++ src/utils/setupServer.ts
@@ -1,5 +1,12 @@
-import { Application } from "express";
+import compression from "compression";
+import session from "cookie-session";
+import cors from "cors";
+import { Application, json, urlencoded } from "express";
+import "express-async-errors";
+import helmet from "helmet";
+import hpp from "hpp";
 import { Server } from "http";
+import { WEEK } from "src/constants";
 
 class ChatServer {
 	private app: Application;
@@ -16,8 +23,32 @@ class ChatServer {
 		this.startServer(this.app);
 	}
 
-	private securityMiddleware(app: Application): void {}
-	private standardMiddleware(app: Application): void {}
+	private securityMiddleware(app: Application): void {
+		app.use(
+			session({
+				name: "newSession",
+				keys: ["test-key"],
+				maxAge: 1 * WEEK,
+				secure: false // TODO: Change when in prod
+			})
+		);
+
+		app.use(hpp()); // Protect against HTTP Parameter Pollution attacks
+		app.use(helmet()); // Sets various HTTP headers
+		app.use(
+			cors({
+				origin: "*",
+				credentials: true,
+				optionsSuccessStatus: 200,
+				methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+			})
+		);
+	}
+	private standardMiddleware(app: Application): void {
+		app.use(compression()); // Compress response bodies for all requests based on the given options
+		app.use(json({ limit: "50mb" }));
+		app.use(urlencoded({ extended: true, limit: "50mb" }));
+	}
 	private routeMiddleware(app: Application): void {}
 	private globalErrorHandler(app: Application): void {}
 	private startServer(app: Application): void {}
